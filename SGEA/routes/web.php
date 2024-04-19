f1ff4019838b2e6432d32289c1a7a151eab00060
Emanuel Beltran <emmanuelbeltranc@gmail.com>
2024-04-18 22:31:05 -0500

    ajustar ruta de artistas

--- SGEA/routes/web.php
+++ SGEA/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artistas',[ArtistasController::class,'index']);
+Route::post('/artistas', [ArtistasController::class, 'store'])->name('artistas.store');
+Route::get('/artistas/create', [ArtistasController::class, 'create'])->name('artistas.create');
+Route::delete('/artistas/{artista}', [ArtistasController::class, 'destroy'])->name('artistas.destroy');
+Route::put('/artistas/{artista}', [ArtistasController::class, 'update'])->name('artistas.update');
+Route::get('/artistas/{artista}/edit', [ArtistasController::class, 'edit'])->name('artistas.edit');
